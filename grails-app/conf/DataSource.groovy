a0947a57331a0b2dc6e4afacd86f938c53a05866
krishnakant singh <searchkisu@gmail.com>
2016-07-25 03:28:36 +0400

    config changes

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -17,8 +17,8 @@ hibernate {
 environments {
     development {
         dataSource {
-            dbCreate = "create-drop" // one of 'create', 'create-drop', 'update', 'validate', ''
-            url = "jdbc:h2:mem:devDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            dbCreate = "" // one of 'create', 'create-drop', 'update', 'validate', ''
+            url = ""
         }
     }
     test {
@@ -37,8 +37,8 @@ environments {
     }
     production {
         dataSource {
-            dbCreate = "update"
-            url = "jdbc:h2:prodDb;MVCC=TRUE;LOCK_TIMEOUT=10000;DB_CLOSE_ON_EXIT=FALSE"
+            dbCreate = ""
+            url = ""
             properties {
                // See http://grails.org/doc/latest/guide/conf.html#dataSource for documentation
                jmxEnabled = true
